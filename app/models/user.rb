4736cddb75cc006b1c2b8338a2bb904060db101c
yukimy819 <yukimy819@gmail.com>
2024-06-13 23:13:47 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
 end
