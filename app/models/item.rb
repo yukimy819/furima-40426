44099e122dfad5159fa84d672e7d02471ddecd4d
yukimy819 <yukimy819@gmail.com>
2024-06-22 18:28:45 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_date
   has_one_attached :image
 
-  validates :item_name, :price, :description, :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, presence: true
-  validates :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_name, :description, :image, :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
 end
