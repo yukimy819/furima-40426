50f87ec48fd51115ceff58bfdbf4f57cbf36e752
yukimy819 <yukimy819@gmail.com>
2024-06-26 22:23:11 +0900

    訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  get 'items/new', to: 'items#new'
   resources :items, only: [:index, :new, :create]
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,53 +11,93 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
-    context '商品が出品ツイートが投稿できない場合' do
-      it '画像が空では投稿できない' do
+    context '商品が出品できない場合' do
+      it '画像が空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it '商品名が空では投稿できない' do
+      it '商品名が空では出品できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
-      it '商品の説明が空では投稿できない' do
+      it '商品の説明が空では出品できない' do
         @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'カテゴリーが空では投稿できない' do
+      it 'カテゴリーが空では出品できない' do
         @item.category_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it '商品の状態が空では投稿できない' do
+      it '商品の状態が空では出品できない' do
         @item.condition_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it '配送料の負担が空では投稿できない' do
+      it '配送料の負担が空では出品できない' do
         @item.delivery_fee_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
-      it '発送元の地域が空では投稿できない' do
+      it '発送元の地域が空では出品できない' do
         @item.area_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Area can't be blank")
       end
-      it '発送までの日数が空では投稿できない' do
+      it '発送までの日数が空では出品できない' do
         @item.delivery_date_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
-      it '販売価格が空では投稿できない' do
+      it '販売価格が空では出品できない' do
         @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
-      end 
-      it 'ユーザーが紐付いていなければ投稿できない' do
+      end
+      it 'カテゴリーに『---』が選択されている場合は出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態に『---』が選択されている場合は出品できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担に『---』が選択されている場合は出品できない' do
+        @item.delivery_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
+      it '発送元の地域に『---』が選択されている場合は出品できない' do
+        @item.area_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it '発送までの日数に『---』が選択されている場合は出品できない' do
+        @item.delivery_date_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+      end
+      it '販売価格は整数でないと出品できない' do
+        @item.price = '25.5'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
+      it '販売価格は300円以上でないと出品できない' do
+        @item.price = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '販売価格は9999999円以下でないと出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'ユーザーが紐付いていなければ出品できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
