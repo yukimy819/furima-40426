67eaec15477912c7983399902a24e9e94cd04098
yukimy819 <yukimy819@gmail.com>
2024-07-03 22:12:46 +0900

    訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,10 @@ class Item < ApplicationRecord
   belongs_to :delivery_date
   has_one_attached :image
 
-  validates :item_name, :description, :image, :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_name, :description, :image, :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id,
+            presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, :condition_id, :delivery_fee_id, :area_id, :delivery_date_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end
