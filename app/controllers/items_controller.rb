f954f1841a62afd91907dd780d9a2c40367e5926
yukimy819 <yukimy819@gmail.com>
2024-07-07 17:40:13 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-
+  before_action :set_item, only: [:edit, :show, :update]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,22 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     return unless current_user != @item.user
-
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
-      render :edit, status: :unprocessable_entity, locals: { model: @item }
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -43,5 +40,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :delivery_fee_id, :area_id,
                                  :delivery_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
