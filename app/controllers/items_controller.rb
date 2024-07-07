8461968496b1370b3a870449aa41fc9707809823
yukimy819 <yukimy819@gmail.com>
2024-07-07 18:43:13 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless current_user != @item.user
+    return unless current_user == @item.user
 
     @item.destroy
     redirect_to root_path
