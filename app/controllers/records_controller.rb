6f9de97b78fec180e8653946ac2f1c8afe100a8f
yukimy819 <yukimy819@gmail.com>
2024-07-14 16:06:09 +0900

    recordコントローラーの記述

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,11 +2,26 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @record_address = RecordAddress.new
   end
 
   def new
+    @record_address = RecordAddress.new
   end
 
   def create
+    @record_address = RecordAddress.new(record_params)
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record_address).permit(:postal_code, :area, :municipality, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items
-  resources :records, only: [:index, :new, :create]
+  resources :items do
+    resources :records, only: [:index, :new, :create]
+  end
 end
