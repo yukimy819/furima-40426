b8d32d29907a809aa18d8784af77dcef5271bfc0
yukimy819 <yukimy819@gmail.com>
2024-07-14 15:38:00 +0900

    addressモデルの作成

---
+++ db/migrate/20240714063021_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string      :postal_code,    null: false
+      t.integer     :area,           null: false
+      t.string      :municipality,   null: false
+      t.string      :street_address, null: false
+      t.string      :building_name
+      t.string      :phone_number,   null: false
+      t.references  :record,         null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
